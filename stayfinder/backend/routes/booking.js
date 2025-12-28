b6edc75f1576840d623a676dd4f64a98da0ce9b9
Rishabh028 <rishabhrajak2004@gmail.com>
2025-12-28 19:46:36 +0530

    Clean repo: SwasthAI project only

--- stayfinder/backend/routes/booking.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bookingController = require('../controllers/bookingController');
-const auth = require('../middleware/auth'); // You need auth middleware for JWT
-
-router.post('/', auth, bookingController.createBooking);
-router.get('/', auth, bookingController.getMyBookings);
-
-module.exports = router;
